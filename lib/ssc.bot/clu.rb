8cea8d6013e2e2024c5f1e45059b0679c3353a6f
esotericpig <esotericpig@users.noreply.github.com>
2020-09-10 15:03:08 +0800

    Initial commit of Clu class

---
+++ lib/ssc.bot/clu.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+# frozen_string_literal: true
+
+#--
+# This file is part of SSC.Bot.
+# Copyright (c) 2020 Jonathan Bradley Whited (@esotericpig)
+# 
+# SSC.Bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# SSC.Bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with SSC.Bot.  If not, see <https://www.gnu.org/licenses/>.
+#++
+
+
+require 'forwardable'
+
+require 'ssc.bot/chat_log'
+require 'ssc.bot/user'
+
+
+module SSCBot
+  ###
+  # Chat Log + User
+  # 
+  # @author Jonathan Bradley Whited (@esotericpig)
+  # @since  0.1.2
+  ###
+  class Clu
+    attr_reader :chat_log
+    attr_reader :msg_sender
+    
+    def initialize(chat_log,msg_sender)
+      extend Forwardable
+      
+      super()
+      
+      @chat_log = chat_log
+      @msg_sender = msg_sender
+      
+      def_delegators(:@chat_log,*(@chat_log.methods - methods))
+      def_delegators(:@msg_sender,*(@msg_sender.methods - methods))
+    end
+  end
+end
